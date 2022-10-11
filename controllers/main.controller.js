bc143c22567e72d9d351e33744baf12d3aa9c34c
Christian Ayala <devchristian91@gmail.com>
2022-10-11 14:29:07 -0500

    ADD jsonwebtoken to sing user for login

--- controllers/main.controller.js
+++ controllers/main.controller.js
@@ -1,6 +1,20 @@
+const CustomAPIError = require('../errors/custom-error');
+const JWT = require('jsonwebtoken');
+
 const login = async (req, res) => {
-  res.send('Fake Login/Register/Signup Route');
-  console.log(req.body);
+  const { username, password } = req.body;
+  //* Mongoose validation
+  //* Joi
+  //* check in the controller
+  if (!username || !password) {
+    throw new CustomAPIError('Please provide email and password', 400);
+  }
+  //! is demo th DB provided us!!
+  const id = new Date().getDate();
+
+  const token = JWT.sign({id, username}, process.env.JWT_SECRET, {expiresIn:'30d'});
+
+  res.status(200).json({msg:'User created', token})
 };
 
 const dashboard = async (req, res) => {
